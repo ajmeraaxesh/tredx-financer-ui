31353c5ac92dfd191b464c39bceb27616616b030
Axesh R. Ajmera <ajmeraaxesh@gmail.com>
2022-12-16 11:10:06 +0530

    updated login and register component

---
+++ app/routes/sharedcomponents.tsx
@@ -0,0 +1,9 @@
+import {PrimaryButton, SecondaryButton} from 'ui'
+
+export default function Index() {
+    return (
+        <div className="flex justify-center">
+            <PrimaryButton  />
+        </div>
+    )
+}
